3f1692126c6d3347a73c0160ea470dd0852463c6
WilfredoAaronSosaRamosBoushtech <wilfredo.sosa@boushtech.net>
2024-08-15 10:49:59 -0500

    new main page imp.

---
+++ app/markdown/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react';
+import MarkdownForm from '@/components/MarkdownForm';
+
+const MarkdownPage: React.FC = () => {
+  return (
+      <MarkdownForm darkMode={false} />
+  );
+};
+
+export default MarkdownPage;
