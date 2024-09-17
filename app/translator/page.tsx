9cd01df37e7eb719aa0f9e3e53cc260978a95ac1
WilfredoAaronSosaRamosBoushtech <wilfredo.sosa@boushtech.net>
2024-09-17 18:46:36 -0500

    new feature/translation imp.

---
+++ app/translator/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import TranslationForm from '@/components/TranslatorForm';
+import React from 'react';
+
+const JsonToSQLPage: React.FC = () => {
+  return (
+      <TranslationForm darkMode={false} />
+  );
+};
+
+export default JsonToSQLPage;
